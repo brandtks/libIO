f9c700f0324cfac5d6367ea82e9f4658b8ef41f3
AquaPic <brandtsa@live.com>
2013-05-20 23:32:26 -0400

    added GPL license

--- libIO.h
+++ libIO.h
@@ -1,22 +1,23 @@
 /*
 	NOT TESTED!!
-	libIO - A I2C extended IO for Texas Instrurments PCF8574.
+	libDAC - An I2C Extended IO Library for chipKit.
+	Texas Instrurments PCF8574 over I2C serial communications.
 
 	Created by Skyler Brandt on May 2013 for chipKit.
 
-	This library is free software; you can redistribute it and/or
-	modify it under the terms of the GNU Lesser General Public
-	License as published by the Free Software Foundation; either
-	version 2.1 of the License, or (at your option) any later version.
+	Copyright 2013 Skyler Brandt
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-	This library is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-	Lesser General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-	You should have received a copy of the GNU Lesser General Public
-	License along with this library; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/
 */
 
 #ifndef LIB_IO_H
