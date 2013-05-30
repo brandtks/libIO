a600dcc9dc917ff7ca74e0031266fc9905eb337c
AquaPic <brandtsa@live.com>
2013-05-29 20:42:58 -0400

    tested, added a few methods, added README.txt, added an example

--- libIO.cpp
+++ libIO.cpp
@@ -1,43 +1,43 @@
 /*
-	NOT TESTED!!
-	libDAC - An I2C Extended IO Library for chipKit.
-	Texas Instrurments PCF8574 over I2C serial communications.
+  libDAC - An I2C Extended IO Library for chipKit.
+  Texas Instrurments PCF8574 over I2C serial communications.
 
-	Created by Skyler Brandt on May 2013 for chipKit.
+  Created by Skyler Brandt on May 2013 for chipKit.
 
-	Copyright 2013 Skyler Brandt
-	This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+  Copyright 2013 Skyler Brandt
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/
 */
 
 #include <inttypes.h>
+#include <WProgram.h>
 #include <Wire.h>
 #include <libIO.h>
-#include <WProgram.h>
 
 /******************************************************************************
  * Class Variables
  ******************************************************************************/
 int libIO::address = 0;
-uint8_t libIO::outReg = 0;
-boolean libIO::out[8] = {false, false, false, false, false, false, false, false};
+uint8_t libIO::outReg = 0;  //Register sent to device
+bool libIO::out[8] = {true, true, true, true, true, true, true, true};
+uint8_t libIO::inReg = 0;
 
 /******************************************************************************
  * Constructors
  ******************************************************************************/
 libIO::libIO(int initAddress)
 {
-	address = initAddress;
+  address = initAddress;
 }
 
 /******************************************************************************
@@ -45,21 +45,17 @@ libIO::libIO(int initAddress)
  ******************************************************************************/
 
 /**********************************************************
- *setup the device
+ *setup the device to use as an output (0) or input (1)
  **********************************************************/
 void libIO::init(char inOut)
 {
-	if (inOut == 1)
+    if (inOut == 1)
 	{
-		Wire.beginTransmission(address);
-		Wire.send(0xFF);
-		Wire.endTransmission();
+		allOn();
 	}
-	else
+	else if (inOut == 0)
 	{
-		Wire.beginTransmission(address);
-		Wire.send(0x00);
-		Wire.endTransmission();
+		allOff();
 	}
 }
 
@@ -68,49 +64,71 @@ void libIO::init(char inOut)
  **********************************************************/
 void libIO::setOutOn(char bit)
 {
-	//add binary bit to byte being sent
-	switch(bit)
-	{
-		case 0:
-			outReg += 1;
-			out[0] = true;
-			break;
-		case 1:
-			outReg += 2;
-			out[1] = true;
-			break;
-		case 2:
-			outReg += 4;
-			out[2] = true;
-			break;
-		case 3:
-			outReg += 8;
-			out[3] = true;
-			break;
-		case 4:
-			outReg += 16;
-			out[4] = true;
-			break;
-		case 5:
-			outReg += 32;
-			out[5] = true;
-			break;
-		case 6:
-			outReg += 64;
-			out[6] = true;
-			break;
-		case 7:
-			outReg += 128;
-			out[7] = true;
-			break;
-		default:
-			break;
-	}
+  //add binary bit to byte being sent
+  switch(bit)
+  {
+  case 0:
+    if (!out[0])
+    {
+      outReg += 1;
+      out[0] = true;
+    }
+    break;
+  case 1:
+    if (!out[1])
+    {
+      outReg += 2;
+      out[1] = true;
+    }
+    break;
+  case 2:
+    if (!out[2])
+    {
+      outReg += 4;
+      out[2] = true;
+    }
+    break;
+  case 3:
+    if (!out[3])
+    {
+      outReg += 8;
+      out[3] = true;
+    }
+    break;
+  case 4:
+    if (!out[4])
+    {
+      outReg += 16;
+      out[4] = true;
+    }
+    break;
+  case 5:
+    if (!out[5])
+    {
+      outReg += 32;
+      out[5] = true;
+    }
+    break;
+  case 6:
+    if (!out[6])
+    {
+      outReg += 64;
+      out[6] = true;
+    }
+    break;
+  case 7:
+    if (!out[7])
+    {
+      outReg += 128;
+      out[7] = true;
+    }
+    break;
+  default:
+    break;
+  }
 
-	//send byte
-	Wire.beginTransmission(address);
-	Wire.send(outReg);
-	Wire.endTransmission();
+  //send byte
+  setIO(outReg);
 }
 
 /**********************************************************
@@ -118,102 +136,178 @@ void libIO::setOutOn(char bit)
  **********************************************************/
 void libIO::setOutOff(char bit)
 {
-	//subtract binary bit from byte being sent
-	switch(bit)
-	{
-		case 0:
-			if (out[0])
-			{
-				outReg -= 1;
-				out[0] = false;
-			}
-			break;
-		case 1:
-			if (out[1])
-			{
-				outReg -= 2;
-				out[1] = false;
-			}
-			break;
-		case 2:
-			if (out[2])
-			{
-				outReg -= 4;
-				out[2] = false;
-			}
-			break;
-		case 3:
-			if (out[3])
-			{
-				outReg -= 8;
-				out[3] = false;
-			}
-			break;
-		case 4:
-			if (out[4])
-			{
-				outReg -= 16;
-				out[4] = false;
-			}
-			break;
-		case 5:
-			if (out[5])
-			{
-				outReg -= 32;
-				out[5] = false;
-			}
-			break;
-		case 6:
-			if (out[6])
-			{
-				outReg -= 64;
-				out[6] = false;
-			}
-			break;
-		case 7:
-			if (out[7])
-			{
-				outReg -= 128;
-				out[7] = false;
-			}
-			break;
-		default:
-			break;
-	}
+  //subtract binary bit from byte being sent
+  switch(bit)
+  {
+  case 0:
+    if (out[0])
+    {
+      outReg -= 1;
+      out[0] = false;
+    }
+    break;
+  case 1:
+    if (out[1])
+    {
+      outReg -= 2;
+      out[1] = false;
+    }
+    break;
+  case 2:
+    if (out[2])
+    {
+      outReg -= 4;
+      out[2] = false;
+    }
+    break;
+  case 3:
+    if (out[3])
+    {
+      outReg -= 8;
+      out[3] = false;
+    }
+    break;
+  case 4:
+    if (out[4])
+    {
+      outReg -= 16;
+      out[4] = false;
+    }
+    break;
+  case 5:
+    if (out[5])
+    {
+      outReg -= 32;
+      out[5] = false;
+    }
+    break;
+  case 6:
+    if (out[6])
+    {
+      outReg -= 64;
+      out[6] = false;
+    }
+    break;
+  case 7:
+    if (out[7])
+    {
+      outReg -= 128;
+      out[7] = false;
+    }
+    break;
+  default:
+    break;
+  }
 
-	//send byte
-	Wire.beginTransmission(address);
-	Wire.send(outReg);
-	Wire.endTransmission();
+  //send byte
+  setIO(outReg);
 }
 
 /**********************************************************
  *turn all outputs off
  **********************************************************/
-uint8_t libIO::allOff()
+void libIO::allOff()
 {
-	Wire.beginTransmission(address);
-	Wire.send(0x00);
-	Wire.endTransmission();
+  outReg = 0x00;
+  setIO(outReg);
+  for (char i=0 ; i<8 ; i++)
+  {
+      out[i] = false;
+  }
+}
+
+/**********************************************************
+ *turn all outputs on
+ **********************************************************/
+void libIO::allOn()
+{
+  outReg = 0xFF;
+  setIO(outReg);
+  for (char i=0 ; i<8 ; i++)
+  {
+      out[i] = true;
+  }
 }
 
 /**********************************************************
  *check specific bit status
  **********************************************************/
-boolean libIO::bitStat(char bit)
+bool libIO::bitStat(char bit)
 {
-	uint8_t byte = inputReg();
+  uint8_t inByte = inputReg(); //get input register from device
+  inReg = inByte;  //save to class variable
+  inByte >>= bit;  //shift off bits
+  inByte = inByte & 0x01;
+  if (inByte == 0x01)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
 
-	byte >>= bit;
-	byte = byte & 0x01;
-	if (byte == 0x01)
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
+bool libIO::bitStat(char bit, bool update)
+{
+  uint8_t byte = 0;
+
+  if (update)
+  {
+    byte = inputReg();  //get input register from device
+    inReg = byte;  //save to class variable
+  }
+  else
+  {
+    byte = inReg;  //used if already updated input register on that scan and want to check the status of another bit
+  }
+  byte >>= bit;  //shift off bits
+  byte = byte & 0x01;
+  if (byte == 0x01)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+/**********************************************************
+ *returns pointer to an array of booleans on the status of the input byte
+ *  use only if entire device is used for inputs
+ **********************************************************/
+bool* libIO::inputArray()
+{
+  static bool in[] = {false, false, false, false, false, false, false, false};
+  uint8_t byte = inputReg(); //get input register from device
+  uint8_t tempByte = 0;
+  for (char i=0 ; i<0 ; i++)
+  {
+    tempByte = byte;
+    tempByte >>= i;
+    tempByte = byte & 0x01;
+    if (tempByte == 0x01)
+    {
+      in[i] = true;
+    }
+  }
+  return in;
+}
+
+/******************************************************************************
+ *Private Functions
+ ******************************************************************************/
+
+/**********************************************************
+ *send output Registers via I2C to device
+ **********************************************************/
+void libIO::setIO(uint8_t outputReg)
+{
+  uint8_t sendByte[2] = {0, outputReg};
+  Wire.beginTransmission(address);
+  Wire.send(sendByte, 2);
+  Wire.endTransmission();
 }
 
 /**********************************************************
@@ -221,14 +315,13 @@ boolean libIO::bitStat(char bit)
  **********************************************************/
 uint8_t libIO::inputReg()
 {
-	uint8_t byte = 0;
+  uint8_t registerByte = 0;
+  Wire.requestFrom(address,2);
+  while(Wire.available())
+  {
+    registerByte = Wire.receive();
+    registerByte = Wire.receive();
+  }
 
-	Wire.requestFrom(address,1);
-	while(Wire.available())
-	{
-		byte = Wire.receive();
-	}
-
-	return byte;
+  return registerByte;
 }
-
--- libIO.h
+++ libIO.h
@@ -1,23 +1,22 @@
 /*
-	NOT TESTED!!
-	libDAC - An I2C Extended IO Library for chipKit.
-	Texas Instrurments PCF8574 over I2C serial communications.
+  libDAC - An I2C Extended IO Library for chipKit.
+  Texas Instrurments PCF8574 over I2C serial communications.
 
-	Created by Skyler Brandt on May 2013 for chipKit.
+  Created by Skyler Brandt on May 2013 for chipKit.
 
-	Copyright 2013 Skyler Brandt
-	This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+  Copyright 2013 Skyler Brandt
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/
 */
 
 #ifndef LIB_IO_H
@@ -27,15 +26,25 @@
 
 class libIO
 {
-	private:
+private:
+  static int address;
+  static uint8_t outReg;
+  static bool out[];
+  static uint8_t inReg;
+  static void setIO(uint8_t outputReg);
+  static uint8_t inputReg();
 
-	public:
-		void init(char inOut);
-		void setOutOn(char bit);
-		void setOutOff(char bit);
-		void allOff();
-		boolean bitStat(char bit);
-		uint8_t inputReg();
+public:
+  libIO(int initAddress);
+  void init(char inOut);
+  void setOutOn(char bit);
+  void setOutOff(char bit);
+  void allOff();
+  void allOn();
+  bool bitStat(char bit);
+  bool bitStat(char bit, bool update);
+  bool* inputArray();
 };
 
 #endif
+
