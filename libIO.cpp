32a16edc468cb0b5a0c975448725800c50c8c703
AquaPic <brandtsa@live.com>
2013-07-03 12:49:23 -0400

    added setAddress()

--- libIO.cpp
+++ libIO.cpp
@@ -29,17 +29,26 @@
  ******************************************************************************/
 libIO::libIO(int initAddress)
 {
-  address = initAddress;  //address of the device
-	for (uint8_t i=0 ; i<8 ; i++)
-	{
-		out[i] = true;
-	}
+	initiate(initAddress);
+}
+
+libIO::libIO()
+{
+	initiate(-1);
 }
 
 /******************************************************************************
  * Global Functions
  ******************************************************************************/
 
+/**********************************************************
+ *set instance device address
+ **********************************************************/ 
+ void libDAC::setAddress(int initAddress)
+ {
+	address = initAddress;
+ }
+ 
 /**********************************************************
  *setup the device to use as an output (1) or input (0)
  **********************************************************/
@@ -79,12 +88,12 @@ void libIO::writeOut(uint8_t bit, uint8_t state)
  **********************************************************/
 void libIO::allOff()
 {
-  outReg = 0x00;
-  setIO(outReg);
-  for (uint8_t i=0 ; i<8 ; i++)
-  {
+	outReg = 0x00;
+	setIO(outReg);
+	for (uint8_t i=0 ; i<8 ; i++)
+	{
 		out[i] = false;
-  }
+	}
 }
 
 /**********************************************************
@@ -92,12 +101,12 @@ void libIO::allOff()
  **********************************************************/
 void libIO::allOn()
 {
-  outReg = 0xFF;
-  setIO(outReg);
-  for (uint8_t i=0 ; i<8 ; i++)
-  {
+	outReg = 0xFF;
+	setIO(outReg);
+	for (uint8_t i=0 ; i<8 ; i++)
+	{
 		out[i] = true;
-  }
+	}
 }
 
 /**********************************************************
@@ -115,43 +124,43 @@ bool libIO::getWrittenOutStatus(uint8_t bit)
 //gets input register from device
 uint8_t libIO::bitStat(uint8_t bit)
 {
-  uint8_t inByte = inputReg(); //get input register from device
-  inReg = inByte;  //save to class variable
-  inByte >>= bit;  //shift off bits
-  inByte = inByte & 0x01;  //determine if the first bit is a 1 or 0
-  if (inByte == 0x01)
-  {
-    return HIGH;
-  }
-  else
-  {
-    return LOW;
-  }
+	uint8_t inByte = inputReg(); //get input register from device
+	inReg = inByte;  //save to class variable
+	inByte >>= bit;  //shift off bits
+	inByte = inByte & 0x01;  //determine if the first bit is a 1 or 0
+	if (inByte == 0x01)
+	{
+		return HIGH;
+	}
+	else
+	{
+		return LOW;
+	}
 }
 
 uint8_t libIO::bitStat(uint8_t bit, bool update)
 {
-  uint8_t byte = 0;
+	uint8_t byte = 0;
 
-  if (update)
-  {
-    byte = inputReg();  //get input register from device
-    inReg = byte;  //save to class variable
-  }
-  else
-  {
-    byte = inReg;  //used if already updated input register on that scan and want to check the status of another bit
-  }
-  byte >>= bit;  //shift off bits
-  byte = byte & 0x01;
-  if (byte == 0x01)
-  {
-    return HIGH;
-  }
-  else
-  {
-    return LOW;
-  }
+	if (update)
+	{
+		byte = inputReg();  //get input register from device
+		inReg = byte;  //save to class variable
+	}
+	else
+	{
+		byte = inReg;  //used if already updated input register on that scan and want to check the status of another bit
+	}
+	byte >>= bit;  //shift off bits
+	byte = byte & 0x01;
+	if (byte == 0x01)
+	{
+		return HIGH;
+	}
+	else
+	{
+		return LOW;
+	}
 }
 
 /**********************************************************
@@ -159,103 +168,115 @@ uint8_t libIO::bitStat(uint8_t bit, bool update)
  **********************************************************/
 uint8_t* libIO::inputArray()
 {
-  uint8_t* in = new uint8_t[8];
+	uint8_t* in = new uint8_t[8];
 	uint8_t tempByte = 0;
   
 	uint8_t byte = inputReg(); //get input register from device
 	inReg = byte;  //save input register to class variable
 
-  for (uint8_t i=0 ; i<8 ; i++)
-  {
-    tempByte = byte;
-    tempByte >>= i;
-    tempByte = tempByte & 0x01;
-    if (tempByte == 0x01)
-    {
-      in[i] = HIGH;
-    }
+	for (uint8_t i=0 ; i<8 ; i++)
+	{
+		tempByte = byte;
+		tempByte >>= i;
+		tempByte = tempByte & 0x01;
+		if (tempByte == 0x01)
+		{
+			in[i] = HIGH;
+		}
 		else
 		{
 			in[i] = LOW;
 		}
-  }
-  return in;
+	}
+	return in;
 }
 
 /******************************************************************************
  *Private Functions
  ******************************************************************************/
 
+ /**********************************************************
+ *initiate
+ **********************************************************/
+void libIO::initiate(int initAddress)
+{
+	address = initAddress;  //address of the device
+	for (uint8_t i=0 ; i<8 ; i++)
+	{
+		out[i] = true;
+	}
+}
+ 
 /**********************************************************
  *turn single bit output on
  **********************************************************/
 void libIO::setOutOn(uint8_t bit)
 {
-  //add binary bit to byte being sent
-  switch(bit)
-  {
-  case 0:
-    if (!out[0])
-    {
-      outReg += 1;
-      out[0] = true;
-    }
-    break;
-  case 1:
-    if (!out[1])
-    {
-      outReg += 2;
-      out[1] = true;
-    }
-    break;
-  case 2:
-    if (!out[2])
-    {
-      outReg += 4;
-      out[2] = true;
-    }
-    break;
-  case 3:
-    if (!out[3])
-    {
-      outReg += 8;
-      out[3] = true;
-    }
-    break;
-  case 4:
-    if (!out[4])
-    {
-      outReg += 16;
-      out[4] = true;
-    }
-    break;
-  case 5:
-    if (!out[5])
-    {
-      outReg += 32;
-      out[5] = true;
-    }
-    break;
-  case 6:
-    if (!out[6])
-    {
-      outReg += 64;
-      out[6] = true;
-    }
-    break;
-  case 7:
-    if (!out[7])
-    {
-      outReg += 128;
-      out[7] = true;
-    }
-    break;
-  default:
-    break;
-  }
+	//add binary bit to byte being sent
+	switch(bit)
+	{
+	case 0:
+		if (!out[0])
+		{
+			outReg += 1;
+			out[0] = true;
+		}
+		break;
+	case 1:
+		if (!out[1])
+		{
+			outReg += 2;
+			out[1] = true;
+		}
+		break;
+	case 2:
+		if (!out[2])
+		{
+			outReg += 4;
+			out[2] = true;
+		}
+		break;
+	case 3:
+		if (!out[3])
+		{
+			outReg += 8;
+			out[3] = true;
+		}
+		break;
+	case 4:
+		if (!out[4])
+		{
+			outReg += 16;
+			out[4] = true;
+		}
+		break;
+	case 5:
+		if (!out[5])
+		{
+			outReg += 32;
+			out[5] = true;
+		}
+		break;
+	case 6:
+		if (!out[6])
+		{
+			outReg += 64;
+		out[6] = true;
+		}
+		break;
+	case 7:
+		if (!out[7])
+		{
+			outReg += 128;
+			out[7] = true;
+		}
+		break;
+	default:
+		break;
+	}
 
-  //send byte
-  setIO(outReg);
+	//send byte
+	setIO(outReg);
 }
 
 /**********************************************************
@@ -263,71 +284,71 @@ void libIO::setOutOn(uint8_t bit)
  **********************************************************/
 void libIO::setOutOff(uint8_t bit)
 {
-  //subtract binary bit from byte being sent
-  switch(bit)
-  {
-  case 0:
-    if (out[0])
-    {
-      outReg -= 1;
-      out[0] = false;
-    }
-    break;
-  case 1:
-    if (out[1])
-    {
-      outReg -= 2;
-      out[1] = false;
-    }
-    break;
-  case 2:
-    if (out[2])
-    {
-      outReg -= 4;
-      out[2] = false;
-    }
-    break;
-  case 3:
-    if (out[3])
-    {
-      outReg -= 8;
-      out[3] = false;
-    }
-    break;
-  case 4:
-    if (out[4])
-    {
-      outReg -= 16;
-      out[4] = false;
-    }
-    break;
-  case 5:
-    if (out[5])
-    {
-      outReg -= 32;
-      out[5] = false;
-    }
-    break;
-  case 6:
-    if (out[6])
-    {
-      outReg -= 64;
-      out[6] = false;
-    }
-    break;
-  case 7:
-    if (out[7])
-    {
-      outReg -= 128;
-      out[7] = false;
-    }
-    break;
-  default:
-    break;
-  }
+	//subtract binary bit from byte being sent
+	switch(bit)
+	{
+	case 0:
+		if (out[0])
+		{
+			outReg -= 1;
+			out[0] = false;
+		}
+		break;
+	case 1:
+		if (out[1])
+		{
+			outReg -= 2;
+			out[1] = false;
+		}
+		break;
+	case 2:
+		if (out[2])
+		{
+			outReg -= 4;
+			out[2] = false;
+		}
+		break;
+	case 3:
+		if (out[3])
+		{
+			outReg -= 8;
+			out[3] = false;
+		}
+		break;
+	case 4:
+		if (out[4])
+		{
+			outReg -= 16;
+			out[4] = false;
+		}
+		break;
+	case 5:
+		if (out[5])
+		{
+			outReg -= 32;
+			out[5] = false;
+		}
+		break;
+	case 6:
+		if (out[6])
+		{
+			outReg -= 64;
+			out[6] = false;
+		}
+		break;
+	case 7:
+		if (out[7])
+		{
+			outReg -= 128;
+			out[7] = false;
+		}
+		break;
+	default:
+		break;
+	}
 
-  //send byte
-  setIO(outReg);
+	//send byte
+	setIO(outReg);
 }
 
 /**********************************************************
@@ -335,10 +356,10 @@ void libIO::setOutOff(uint8_t bit)
  **********************************************************/
 void libIO::setIO(uint8_t outputReg)
 {
-  uint8_t sendByte[2] = {0, outputReg};
-  Wire.beginTransmission(address);
-  Wire.send(sendByte, 2);
-  Wire.endTransmission();
+	uint8_t sendByte[2] = {0, outputReg};
+	Wire.beginTransmission(address);
+	Wire.send(sendByte, 2);
+	Wire.endTransmission();
 }
 
 /**********************************************************
@@ -346,13 +367,13 @@ void libIO::setIO(uint8_t outputReg)
  **********************************************************/
 uint8_t libIO::inputReg()
 {
-  uint8_t registerByte = 0;
-  Wire.requestFrom(address,2);
-  while(Wire.available())
-  {
-    registerByte = Wire.receive();
-    registerByte = Wire.receive();
-  }
+	uint8_t registerByte = 0;
+	Wire.requestFrom(address,2);
+	while(Wire.available())
+	{
+		registerByte = Wire.receive();
+		registerByte = Wire.receive();
+	}
 
-  return registerByte;
+	return registerByte;
 }
--- libIO.h
+++ libIO.h
@@ -33,26 +33,28 @@
 class libIO
 {
 private:
-  int address;
-  uint8_t outReg;
-  bool out[8];
-  uint8_t inReg;
-  
+	int address;
+	uint8_t outReg;
+	bool out[8];
+	uint8_t inReg;
+	
+	void initiate(int initAddress);
 	void setOutOn(uint8_t bit);
-  void setOutOff(uint8_t bit);  
+	void setOutOff(uint8_t bit);  
 	void setIO(uint8_t outputReg);
-  uint8_t inputReg();
+	uint8_t inputReg();
 
 public:
-  libIO(int initAddress);
-  void init(uint8_t inOut);
+	libIO(int initAddress);
+	libIO();
+	void init(uint8_t inOut);
 	void writeOut(uint8_t bit, uint8_t state);
-  void allOff();
-  void allOn();
+	void allOff();
+	void allOn();
 	bool getWrittenOutStatus(uint8_t bit);
-  uint8_t bitStat(uint8_t bit);
-  uint8_t bitStat(uint8_t bit, bool update);
-  uint8_t* inputArray();
+	uint8_t bitStat(uint8_t bit);
+	uint8_t bitStat(uint8_t bit, bool update);
+	uint8_t* inputArray();
 };
 
 #endif
